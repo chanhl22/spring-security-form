afdf5d907d5136f6c3da37746d1fe55aec70ce3b
chanhl22 <chanhlee@student.42seoul.kr>
2022-03-21 16:13:49 +0900

    message 수정

--- src/main/java/hello/springsecurityform/form/SampleController.java
+++ src/main/java/hello/springsecurityform/form/SampleController.java
@@ -27,7 +27,7 @@ public class SampleController {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
-        model.addAttribute("message", "Hello" + principal.getName());
+        model.addAttribute("message", "Hello, " + principal.getName());
         return "dashboard";
     }
 
