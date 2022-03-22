c3a7902f5a3f39be85dc728fedfdb99371efc4e0
chanhl22 <chanhlee@student.42seoul.kr>
2022-03-22 23:08:49 +0900

    JPA 연동

--- src/main/java/hello/springsecurityform/config/SecurityConfig.java
+++ src/main/java/hello/springsecurityform/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package hello.springsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
@@ -21,10 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("hello").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}!@#").roles("ADMIN");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("hello").password("{noop}123").roles("USER").and()
+//                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+//    }
+
 }
