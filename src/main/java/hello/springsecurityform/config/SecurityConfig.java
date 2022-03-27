3e8c60dedfdfcba20ddac8210b4278fd4b8adb2c
chanhl22 <chanhlee@student.42seoul.kr>
2022-03-27 12:37:09 +0900

    FilterChainProxy

--- src/main/java/hello/springsecurityform/config/SecurityConfig.java
+++ src/main/java/hello/springsecurityform/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package hello.springsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
